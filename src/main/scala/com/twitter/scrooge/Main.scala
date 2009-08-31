f7635b3083e6199bdc695cbc569ca025039fb400
Robey Pointer <robey@twitter.com>
2009-08-31 15:21:55 -0700

    cleanup main to dump to a specified file.

--- src/main/scala/com/twitter/scrooge/Main.scala
+++ src/main/scala/com/twitter/scrooge/Main.scala
@@ -1,12 +1,14 @@
 package com.twitter.scrooge
 
+import _root_.java.io._
+
 object Main {
   def main(args: Array[String]) {
-    println("Hello, world!")
-
     val importer = new Importer(args(0))
     val doc = (new parser.Parser(importer)).parseFile(args(1))
-    println(doc)
-    println(ScalaGen(doc))
+    val text = ScalaGen(doc)
+    val out = new FileOutputStream(new File(args(2)))
+    out.write(text.getBytes())
+    out.close()
   }
 }
