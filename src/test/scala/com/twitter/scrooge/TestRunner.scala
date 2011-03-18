85fee05f6f806d9a63f4154dc75f54e5d22cfaf5
Robey Pointer <robey@twitter.com>
2011-03-18 16:21:14 -0700

    killllll!

--- src/test/scala/com/twitter/scrooge/TestRunner.scala
+++
@@ -1,6 +0,0 @@
-package com.twitter.scrooge
-
-import org.specs.runner.SpecsFileRunner
-
-object TestRunner extends SpecsFileRunner("src/test/scala/**/*.scala", ".*",
-  System.getProperty("system", ".*"), System.getProperty("example", ".*"))
