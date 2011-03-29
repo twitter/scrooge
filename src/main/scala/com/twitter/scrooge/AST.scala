a637eddbac0824537880830406a183218f70fc7e
Ian Ownbey <imownbey@gmail.com>
2011-03-29 15:11:33 -0700

    Added equals method for all case classes with an Array

--- src/main/scala/com/twitter/scrooge/AST.scala
+++ src/main/scala/com/twitter/scrooge/AST.scala
@@ -34,23 +34,84 @@ object AST {
     optional: Boolean)
 
   case class Function(name: String, `type`: FunctionType, args: Array[Field], oneway: Boolean,
-    throws: Array[Field])
+    throws: Array[Field]) {
+    override def equals(other: Any) = {
+      other match {
+        case Function(oName, oType, oArgs, oOneWay, oThrows) => {
+          name == oName &&
+          `type` == oType &&
+          args.toList == oArgs.toList &&
+          oneway == oOneWay &&
+          throws.toList == oThrows.toList
+        }
+        case _ => false
+      }
+    }
+  }
 
   sealed abstract class Definition(val name: String)
   case class Const(override val name: String, `type`: FieldType, value: Constant) extends Definition(name)
   case class Typedef(override val name: String, `type`: DefinitionType) extends Definition(name)
-  case class Enum(override val name: String, values: Array[EnumValue]) extends Definition(name)
+  case class Enum(override val name: String, values: Array[EnumValue]) extends Definition(name) {
+    override def equals(other: Any) = {
+      other match {
+        case Enum(oName, oValues) => {
+          name == oName &&
+          values.toList == oValues.toList
+        }
+        case _ => false
+      }
+    }
+  }
   case class EnumValue(name: String, value: Int)
-  case class Senum(override val name: String, values: Array[String]) extends Definition(name)
+  case class Senum(override val name: String, values: Array[String]) extends Definition(name) {
+    override def equals(other: Any) = {
+      other match {
+        case Senum(oName, oValues) => {
+          name == oName &&
+          values.toList == oValues.toList
+        }
+        case _ => false
+      }
+    }
+  }
   sealed abstract class StructLike(override val name: String, val fields: Array[Field]) extends Definition(name)
-  case class Struct(override val name: String, override val fields: Array[Field]) extends StructLike(name, fields)
-  case class Exception_(override val name: String, override val fields: Array[Field]) extends StructLike(name, fields)
-  case class Service(override val name: String, parent: Option[String], functions: Array[Function]) extends Definition(name)
+  case class Struct(override val name: String, override val fields: Array[Field]) extends StructLike(name, fields) {
+    override def equals(other: Any) = {
+      other match {
+        case Struct(oName, oFields) => name == oName && fields.toList == oFields.toList
+        case _ => false
+      }
+    }
+  }
+  case class Exception_(override val name: String, override val fields: Array[Field]) extends StructLike(name, fields) {
+    override def equals(other: Any) = {
+      other match {
+        case Exception_(oName, oFields) => name == oName && fields.toList == oFields.toList
+        case _ => false
+      }
+    }
+  }
+  case class Service(override val name: String, parent: Option[String], functions: Array[Function]) extends Definition(name) {
+    override def equals(other: Any) = {
+      other match {
+        case Service(oName, oParent, oFunctions) => name == oName && parent == oParent && functions.toList == oFunctions.toList
+        case _ => false
+      }
+    }
+  }
 
   sealed abstract class Header
   case class Include(filename: String, document: Document) extends Header
   case class CppInclude(file: String) extends Header
   case class Namespace(scope: String, name: String) extends Header
 
-  case class Document(headers: Array[Header], defs: Array[Definition])
+  case class Document(headers: Array[Header], defs: Array[Definition]) {
+    override def equals(other: Any) = {
+      other match {
+        case Document(oHeaders, oDefs) => headers.toList == oHeaders.toList && defs.toList == oDefs.toList
+        case _ => false
+      }
+    }
+  }
 }
