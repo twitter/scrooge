c959a56dd47fa78c1e3c2dc65273ab6fc25d8b2f
Robey Pointer <robey@twitter.com>
2011-07-07 01:23:20 -0700

    fix version number and dependencies.

--- project/build/ScroogeProject.scala
+++ project/build/ScroogeProject.scala
@@ -13,10 +13,10 @@ class ScroogeProject(info: ProjectInfo) extends StandardServiceProject(info)
   with NoisyDependencies
   with DefaultRepos
   with SubversionPublisher
-  with IdeaProject
 {
-  val util_core = "com.twitter" % "util-core" % "1.8.11"
-  val util_eval = "com.twitter" % "util-eval" % "1.8.12"
+  val utilVersion = "1.8.12"
+  val util_core = "com.twitter" % "util-core" % utilVersion
+  val util_eval = "com.twitter" % "util-eval" % utilVersion
 
   val libThrift = "thrift" % "libthrift" % "0.5.0"
 
@@ -28,7 +28,7 @@ class ScroogeProject(info: ProjectInfo) extends StandardServiceProject(info)
   val asm = "asm" % "asm" % "1.5.3" % "test"
   val objenesis = "org.objenesis" % "objenesis" % "1.1" % "test"
 
-//  override def mainClass = Some("com.twitter.scrooge.Main")
+  override def mainClass = Some("com.twitter.scrooge.Main")
 
   override def subversionRepository = Some("http://svn.local.twitter.com/maven")
 }
