37bb2178f5ddd10491b28c66b229e15efe1d7668
Jeremy Cloud <jeremycloud@twitter.com>
2012-01-27 17:40:55 -0800

    collect client request stats

--- src/main/resources/scalagen/finagleClient.scala
+++ src/main/resources/scalagen/finagleClient.scala
@@ -1,13 +1,15 @@
 // ----- finagle client
 
 import com.twitter.finagle.{Service => FinagleService}
+import com.twitter.finagle.stats.{NullStatsReceiver, StatsReceiver}
 import com.twitter.finagle.thrift.ThriftClientRequest
 import com.twitter.scrooge.FinagleThriftClient
 
 class FinagledClient(
   {{override}}val service: FinagleService[ThriftClientRequest, Array[Byte]],
   {{override}}val protocolFactory: TProtocolFactory = new TBinaryProtocol.Factory,
-  override val serviceName: Option[String] = None
+  override val serviceName: Option[String] = None,
+  stats: StatsReceiver = NullStatsReceiver
 ) extends {{extends}} with FutureIface {
 {{#functions}}
 {{function}}
--- src/main/resources/scalagen/finagleClientFunction.scala
+++ src/main/resources/scalagen/finagleClientFunction.scala
@@ -1,7 +1,19 @@
+private[this] val _{{name}}Scope = stats.scope("{{name}}")
+private[this] val _{{name}}RequestsCounter = _{{name}}Scope.counter("requests")
+private[this] val _{{name}}SuccessCounter = _{{name}}Scope.counter("success")
+private[this] val _{{name}}FailuresCounter = _{{name}}Scope.counter("failures")
+private[this] val _{{name}}FailuresScope = _{{name}}Scope.scope("failures")
+
 {{functionDecl}} = {
+  _{{name}}RequestsCounter.incr()
   encodeRequest("{{name}}", {{localName}}_args({{argNames}})) flatMap { this.service } flatMap {
     decodeResponse(_, {{localName}}_result.decoder)
   } flatMap { result =>
     {{resultUnwrapper}}
+  } onSuccess { _ =>
+    _{{name}}SuccessCounter.incr()
+  } onFailure { ex =>
+    _{{name}}FailuresCounter.incr()
+    _{{name}}FailuresScope.counter(ex.getClass.getName).incr()
   }
 }
