a8c70c5293e5fada4d76cfc37cdad2c70ac1f290
Aaron Stone <aaron@serendipity.cx>
2012-04-04 00:44:07 -0700

    Issue #9 pretty print exceptions by wrapping in a try/catch, and improve verbose output to better identify which file is being processed.

--- src/main/scala/com/twitter/scrooge/Main.scala
+++ src/main/scala/com/twitter/scrooge/Main.scala
@@ -90,24 +90,35 @@ object Main {
     }
 
     for (inputFile <- thriftFiles) {
-      val inputFileDir = new File(inputFile).getParent
-      val importer = Importer.fileImporter(inputFileDir :: importPaths.toList)
-      val parser = new ScroogeParser(importer)
-      val doc0 = parser.parseFile(inputFile).mapNamespaces(namespaceMappings.toMap)
+      try {
+        if (verbose) print("+ Compiling %s".format(inputFile))
 
-      println(doc0) // FIXME: remove this after debugging AST positional error output
+        val inputFileDir = new File(inputFile).getParent
+        val importer = Importer.fileImporter(inputFileDir :: importPaths.toList)
+        val parser = new ScroogeParser(importer)
+        val doc0 = parser.parseFile(inputFile).mapNamespaces(namespaceMappings.toMap)
+        val outputFile = outputFilename map { new File(_) } getOrElse generator.outputFile(destFolder, doc0, inputFile)
+        val lastModified = importer.lastModified(inputFile).getOrElse(Long.MaxValue)
+        if (skipUnchanged && isUnchanged(outputFile, lastModified)) {
+          if (verbose) print(" (unchanged)")
+        } else {
 
-      val outputFile = outputFilename map { new File(_) } getOrElse generator.outputFile(destFolder, doc0, inputFile)
-      val lastModified = importer.lastModified(inputFile).getOrElse(Long.MaxValue)
-      if (!(skipUnchanged && isUnchanged(outputFile, lastModified))) {
-        if (verbose) println("+ Compiling %s".format(inputFile))
-        val doc1 = TypeResolver().resolve(doc0).document
-        val content = generator(doc1, flags.toSet)
+          val doc1 = TypeResolver().resolve(doc0).document
+          val content = generator(doc1, flags.toSet)
 
-        Option(outputFile.getParentFile).foreach { _.mkdirs() }
-        val out = new FileWriter(outputFile)
-        out.write(content)
-        out.close()
+          Option(outputFile.getParentFile).foreach { _.mkdirs() }
+          val out = new FileWriter(outputFile)
+          out.write(content)
+          out.close()
+        }
+      } catch {
+        case ex: Exception => {
+          if (verbose) println("")
+          System.err.println(inputFile + ": " + ex)
+          System.exit(1)
+        }
+      } finally {
+        if (verbose) println("")
       }
     }
   }
