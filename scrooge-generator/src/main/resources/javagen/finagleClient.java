626622d5b58ce288aeb912e0daa05efa79132255
Robey Pointer <robey@twitter.com>
2012-05-15 15:34:07 -0700

    fix up java code (probably -- i don't think this is tested yet).

--- scrooge-generator/src/main/resources/javagen/finagleClient.java
+++ scrooge-generator/src/main/resources/javagen/finagleClient.java
@@ -1,19 +1,16 @@
-public static class FinagledClient extends {{parent}} implements FutureIface {
+public static class FinagledClient{{#hasParent}} extends {{parent}}{{/hasParent}} implements FutureIface {
   private com.twitter.finagle.Service<ThriftClientRequest, byte[]> service;
   private TProtocolFactory protocolFactory /* new TBinaryProtocol.Factory */;
   private StatsReceiver scopedStats;
 
   public FinagledClient(
-      com.twitter.finagle.Service<ThriftClientRequest, byte[]> service,
-      TProtocolFactory protocolFactory /* new TBinaryProtocol.Factory */,
-      String serviceName,
-      StatsReceiver stats) {
-
+    com.twitter.finagle.Service<ThriftClientRequest, byte[]> service,
+    TProtocolFactory protocolFactory /* new TBinaryProtocol.Factory */,
+    String serviceName,
+    StatsReceiver stats
+  ) {
 {{#hasParent}}
     super(service, protocolFactory, serviceName, stats);
-{{/hasParent}}
-{{^hasParent}}
-    super(service, protocolFactory, serviceName);
 {{/hasParent}}
     this.service = service;
     this.protocolFactory = protocolFactory;
@@ -24,6 +21,49 @@ public static class FinagledClient extends {{parent}} implements FutureIface {
     }
   }
 
+{{^hasParent}}
+  // ----- boilerplate that should eventually be moved into finagle:
+
+  protected ThriftClientRequest encodeRequest(String name, ThriftStruct args) {
+    TMemoryBuffer buf = new TMemoryBuffer(512);
+    TProtocol oprot = protocolFactory.getProtocol(buf);
+
+    oprot.writeMessageBegin(new TMessage(name, TMessageType.CALL, 0));
+    args.write(oprot);
+    oprot.writeMessageEnd();
+
+    byte[] bytes = Arrays.copyOfRange(buf.getArray(), 0, buf.length);
+    return new ThriftClientRequest(bytes, false);
+  }
+
+  protected T decodeResponse<T extends ThriftStruct>(byte[] resBytes, ThriftStructCodec<T> codec) {
+    TProtocol iprot = protocolFactory.getProtocol(new TMemoryInputTransport(resBytes));
+    TMessage msg = iprot.readMessageBegin();
+    try {
+      if (msg.type == TMessageType.EXCEPTION) {
+        Exception exception = TApplicationException.read(iprot);
+        if (exception instanceof SourcedException) {
+          if (serviceName != "") ((SourcedException) exception).serviceName = serviceName;
+        }
+        throw exception;
+      } else {
+        return codec.decode(iprot);
+      }
+    } finally {
+      iprot.readMessageEnd();
+    }
+  }
+
+  protected Exception missingResult(String name) {
+    return new TApplicationException(
+      TApplicationException.MISSING_RESULT,
+      "`" + name + "` failed: unknown result"
+    )
+  }
+
+  // ----- end boilerplate.
+
+{{/hasParent}}
 {{#functions}}
   {{>function}}
 {{/function}}
