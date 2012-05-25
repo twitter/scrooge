b767cb3e2da4073315bfa450bc3f3e3dde9b7e0e
Robey Pointer <robey@twitter.com>
2012-05-25 16:20:57 -0700

    'release commit for scrooge:scrooge:3.0.0'

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object Scrooge extends Build {
   )
 
   val sharedSettings = Seq(
-    version := "3.0.0-SNAPSHOT",
+    version := "3.0.0",
     scalaVersion := "2.9.2",
 
     SubversionPublisher.subversionRepository := Some("https://svn.twitter.biz/maven-public"),
