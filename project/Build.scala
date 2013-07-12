4debb6a346b52e671931e481bad1050a183266b0
Jeff Smick <jeff@twitter.com>
2013-07-12 14:12:18 -0700

    bump version to 3.2.1

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object Scrooge extends Build {
-  val scroogeVersion = "3.2.0"
+  val scroogeVersion = "3.2.1"
   val utilVersion = "6.3.6"
   val finagleVersion = "6.5.0"
 
