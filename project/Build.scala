9fbc93da41ee3a68be163ca307fc907cbbaa566e
Evan Meagher <evm@twitter.com>
2013-10-09 21:27:52 +0000

    [split] bump util to 6.6.1-SNAPSHOT
    RB_ID=204962
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Scrooge extends Build {
   val libVersion = "3.9.1"
-  val utilVersion = "6.5.0"
+  val utilVersion = "6.6.0"
   val finagleVersion = "6.6.2"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
