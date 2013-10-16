d21ce766f45243cff7ab85547a8ce5f38d913f2a
Brian Degenhardt <bdegenhardt@twitter.com>
2013-10-16 16:48:59 +0000

    [split] Finagle 6.7.2 release
    RB_ID=209638

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object Scrooge extends Build {
   val libVersion = "3.9.1"
   val utilVersion = "6.6.0"
-  val finagleVersion = "6.7.1"
+  val finagleVersion = "6.7.2"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
   def finagle(which: String) = "com.twitter" %% ("finagle-"+which) % finagleVersion
