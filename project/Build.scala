4691e16b2790c3c55f32eee6b82e0a2865f8b4b0
Brian Degenhardt <bdegenhardt@twitter.com>
2013-10-17 01:01:36 +0000

    [split] bump scrooge to 3.9.3-SNAPSHOT
    RB_ID=210574
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Scrooge extends Build {
-  val libVersion = "3.9.1"
+  val libVersion = "3.9.2"
   val utilVersion = "6.6.0"
   val finagleVersion = "6.7.2"
 
