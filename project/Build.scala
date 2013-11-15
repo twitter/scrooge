d7fc3031319a1d32b771c7952715c099b3379b5c
Steve Gury <stevegury@twitter.com>
2013-11-15 18:14:25 +0000

    [split] Release util version 6.8.1
    RB_ID=232807
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Scrooge extends Build {
   val libVersion = "3.11.1"
-  val utilVersion = "6.8.0"
+  val utilVersion = "6.8.1"
   val finagleVersion = "6.8.0"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
