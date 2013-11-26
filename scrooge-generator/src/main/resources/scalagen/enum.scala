53650dd37d491716a2bb48e152ab1695802fc5c0
Jeff Smick <jeff@twitter.com>
2013-11-26 17:14:41 +0000

    [split] ensure enums with values of the same name will compile
    RB_ID=240155

--- scrooge-generator/src/main/resources/scalagen/enum.scala
+++ scrooge-generator/src/main/resources/scalagen/enum.scala
@@ -7,7 +7,7 @@ import com.twitter.scrooge.ThriftEnum
 case object {{EnumName}} {
 {{#values}}
   {{valuedocstring}}
-  case object {{name}} extends {{EnumName}} {
+  case object {{name}} extends {{package}}.{{EnumName}} {
     val value = {{value}}
     val name = "{{name}}"
   }
@@ -17,10 +17,10 @@ case object {{EnumName}} {
    * Find the enum by its integer value, as defined in the Thrift IDL.
    * @throws NoSuchElementException if the value is not found.
    */
-  def apply(value: Int): {{EnumName}} = {
+  def apply(value: Int): {{package}}.{{EnumName}} = {
     value match {
 {{#values}}
-      case {{value}} => {{EnumName}}.{{name}}
+      case {{value}} => {{package}}.{{EnumName}}.{{name}}
 {{/values}}
       case _ => throw new NoSuchElementException(value.toString)
     }
@@ -30,27 +30,27 @@ case object {{EnumName}} {
    * Find the enum by its integer value, as defined in the Thrift IDL.
    * Returns None if the value is not found
    */
-  def get(value: Int): Option[{{EnumName}}] = {
+  def get(value: Int): Option[{{package}}.{{EnumName}}] = {
     value match {
 {{#values}}
-      case {{value}} => scala.Some({{EnumName}}.{{name}})
+      case {{value}} => scala.Some({{package}}.{{EnumName}}.{{name}})
 {{/values}}
       case _ => scala.None
     }
   }
 
-  def valueOf(name: String): Option[{{EnumName}}] = {
+  def valueOf(name: String): Option[{{package}}.{{EnumName}}] = {
     name.toLowerCase match {
 {{#values}}
-      case "{{unquotedNameLowerCase}}" => scala.Some({{EnumName}}.{{name}})
+      case "{{unquotedNameLowerCase}}" => scala.Some({{package}}.{{EnumName}}.{{name}})
 {{/values}}
       case _ => scala.None
     }
   }
 
-  lazy val list: List[{{EnumName}}] = scala.List[{{EnumName}}](
+  lazy val list: List[{{package}}.{{EnumName}}] = scala.List[{{package}}.{{EnumName}}](
 {{#values}}
-    {{EnumName}}.{{name}}
+    {{package}}.{{EnumName}}.{{name}}
 {{/values|,}}
   )
 }
