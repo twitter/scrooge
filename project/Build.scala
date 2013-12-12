8390f652e75cadb63c959a8ab690bd78016c8a87
Ruben Oanta <roanta@twitter.com>
2013-12-12 22:08:12 +0000

    [split] bump finagle to 6.10.1-SNAPSHOT
    RB_ID=252319
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object Scrooge extends Build {
   val libVersion = "3.11.1"
   val utilVersion = "6.10.0"
-  val finagleVersion = "6.9.0"
+  val finagleVersion = "6.10.0"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
   def finagle(which: String) = "com.twitter" %% ("finagle-"+which) % finagleVersion
