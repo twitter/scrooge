1846e5bbc705ccb006cfac062c3ba0a4c21b282a
Jeff Smick <jeff@twitter.com>
2014-01-10 17:51:59 +0000

    [split] bump scrooge to 3.12.1-SNAPSHOT
    RB_ID=264761
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Scrooge extends Build {
-  val libVersion = "3.11.2"
+  val libVersion = "3.12.0"
   val utilVersion = "6.10.0"
   val finagleVersion = "6.10.0"
 
