3b3d29db7d02964b58e58d0d90e822e9983aa62e
Steve Gury <stevegury@twitter.com>
2014-02-15 04:16:28 +0000

    [split] release finagle version 6.12.0
    RB_ID=289233
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object Scrooge extends Build {
   val libVersion = "3.12.2"
   val utilVersion = "6.12.0"
-  val finagleVersion = "6.11.1"
+  val finagleVersion = "6.12.0"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
   def finagle(which: String) = "com.twitter" %% ("finagle-"+which) % finagleVersion
