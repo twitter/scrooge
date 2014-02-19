40e47855f9f36dbffd257ade9b6f7358d14e79cc
Steve Gury <stevegury@twitter.com>
2014-02-19 07:04:07 +0000

    [split] release scrooge version 3.12.3
    RB_ID=290643
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object Scrooge extends Build {
-  val libVersion = "3.12.2"
+  val libVersion = "3.12.3"
   val utilVersion = "6.12.1"
   val finagleVersion = "6.12.1"
 
