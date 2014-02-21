2a25ec54be30ff19a173dadc5dec2314df8ecda6
Colin Marc <colinmarc@gmail.com>
2014-02-21 18:23:19 +0100

    add 'use' to the list of reserved keywords to escape

--- scrooge-generator/src/main/scala/com/twitter/scrooge/backend/ScalaGenerator.scala
+++ scrooge-generator/src/main/scala/com/twitter/scrooge/backend/ScalaGenerator.scala
@@ -47,7 +47,7 @@ class ScalaGenerator(
       "false", "final", "finally", "for", "forSome", "if", "implicit", "import",
       "lazy", "match", "new", "null", "object", "override", "package", "private",
       "protected", "return", "sealed", "super", "this", "throw", "trait", "try",
-      "true", "type", "val", "var", "while", "with", "yield")
+      "true", "type", "use", "val", "var", "while", "with", "yield")
     def contains(str: String): Boolean = set.contains(str)
   }
 
