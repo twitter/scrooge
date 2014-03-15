0ca19d47e0e11f5b21a2b177fdb453acce2a660b
Mark Drago <mdrago@yodle.com>
2014-03-15 11:54:26 -0400

    Test uses of scala.Product are fully qualified

---
+++ scrooge-generator/src/test/thrift/standalone/product.thrift
@@ -0,0 +1,17 @@
+namespace java thrift.collision
+
+/* Naming a struct Product(N) will collide with scala.Product(N) in a few places.
+ * The victims below will not build properly if uses of scala.Product are not fully qualified. */
+
+struct Product { }
+
+//will not build if collision with Product exists
+struct VictimOfScalaDotProductNameCollision { }
+
+
+struct Product1 { }
+
+//will not build if collision with Product1 exists
+struct VictimOfScalaDotProduct1NameCollision {
+  1: i64 giveVictimArity1
+}
