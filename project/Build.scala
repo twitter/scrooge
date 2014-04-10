7c04058cd1ffa50da76716106d5b69b36dc7a256
Moses Nakamura <mnakamura@twitter.com>
2014-04-10 00:36:44 +0000

    [split] bump util to 6.14.1-SNAPSHOT
    RB_ID=331395
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import net.virtualvoid.sbt.cross.CrossPlugin
 
 object Scrooge extends Build {
   val libVersion = "3.13.1"
-  val utilVersion = "6.13.2"
+  val utilVersion = "6.14.0"
   val finagleVersion = "6.13.1"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
