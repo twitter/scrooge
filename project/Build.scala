e16e510d4fa142553fe149dd4834747bf27f1618
Moses Nakamura <mnakamura@twitter.com>
2014-08-06 00:54:29 +0000

    [split] bump all the things
    RB_ID=422405
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -7,9 +7,9 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 import net.virtualvoid.sbt.cross.CrossPlugin
 
 object Scrooge extends Build {
-  val libVersion = "3.16.1"
-  val utilVersion = "6.18.2"
-  val finagleVersion = "6.19.0"
+  val libVersion = "3.16.2"
+  val utilVersion = "6.18.3"
+  val finagleVersion = "6.20.0"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
   def finagle(which: String) = "com.twitter" %% ("finagle-"+which) % finagleVersion
