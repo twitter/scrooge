twitter/scrooge

commit cad09af7963f01a3efbb8d5a5add8e7354bda1aa

[split] scrooge: breaking out finagle, higher-kinded-type interface Highlights of this change:
1) The finagle service and client classes, plus the deprecated ostrich wrapper trait, are now generated in separate files from the main service interface file.  The main advantage of this is that if you have multiple compiled_idl targets generating from the same thrift but with different options, the source code is purely additive and you don't get conflicts (this isn't entirely true yet, see next bullet point).  This can help reduce the need to generate the same idl into unique namespaces.  For service X, the new class names are X$FinagleClient, X$FinagleService, and X$ThriftServer.

2) I tried to find a completely backwards-compatible way to do the above, but couldn't, so for now, to ease migration, nested classes within the service companion object with the same names as before are still generated, but deprecated.  Once we get everyone switched over to using the new class names, we can stop generating the nested classes.

3) We are no longer generating the sync Iface trait, no one was using it, and it only complicates the code to have two incompatible interfaces to the same service.

4) scrooge now generates a higher-kinded-type interface to abstract away the use of Future from the primary service interface.  For example, we now generate:

       trait TweetService[+MM[_]] extends ThriftService {
         def get(tweetId: Long): MM[Tweet]
       }

    TweetService$FinagleClient and TweetService$FinagleService would now implement TweetService[Future] instead of TweetService.FutureIface, which hard-codes the monadic type.  This will make things easier when com.twitter.util.Future eventually extends scala.concurrent.Future, as you will be able to write code using the TweetService[scala.concurrent.Future] type which would be satisfied by TweetService$FinagleClient, even if the latter still used com.twitter.util.Future.

scrooge-generator/src/main/resources/javagen/service.java
@@ -16,30 +16,25 @@ import java.util.HashSet;
 import org.apache.thrift.protocol.*;
 import org.apache.thrift.TApplicationException;
 {{#withFinagle}}
-import com.twitter.util.Future;
-import com.twitter.util.FutureEventListener;
-{{/withFinagle}}
-{{#withFinagleClient}}
+import com.twitter.finagle.Service;
 import com.twitter.finagle.SourcedException;
+import com.twitter.finagle.stats.Counter;
 import com.twitter.finagle.stats.NullStatsReceiver;
 import com.twitter.finagle.stats.StatsReceiver;
 import com.twitter.finagle.thrift.ThriftClientRequest;
+import com.twitter.util.Function2;
+import com.twitter.util.Function;
+import com.twitter.util.Future;
+import com.twitter.util.FutureEventListener;
 import java.util.Arrays;
 import org.apache.thrift.TException;
-{{/withFinagleClient}}
-{{#withFinagleService}}
-import com.twitter.finagle.Service;
-import com.twitter.finagle.stats.Counter;
-import com.twitter.util.Function;
-import com.twitter.util.Function2;
 import org.apache.thrift.transport.TMemoryBuffer;
 import org.apache.thrift.transport.TMemoryInputTransport;
 import org.apache.thrift.transport.TTransport;
-{{/withFinagleService}}
+{{/withFinagle}}
 {{#withOstrichServer}}
 import com.twitter.finagle.builder.Server;
 import com.twitter.finagle.builder.ServerBuilder;
-import com.twitter.finagle.stats.StatsReceiver;
 import com.twitter.finagle.thrift.ThriftServerFramedCodec;
 import com.twitter.finagle.tracing.NullTracer;
 import com.twitter.finagle.tracing.Tracer;
@@ -49,14 +44,14 @@ import com.twitter.logging.Logger;
 {{docstring}}
 @javax.annotation.Generated(value = "com.twitter.scrooge.Compiler", date = "{{date}}")
 public class {{ServiceName}} {
-  public interface Iface {{syncExtends}}{
+  public interface Iface {{#syncParent}}extends {{syncParent}} {{/syncParent}}{
 {{#syncFunctions}}
     {{>function}};
 {{/syncFunctions}}
   }
 
 {{#withFinagle}}
-  public interface FutureIface {{asyncExtends}}{
+  public interface FutureIface {{#futureIfaceParent}}extends {{futureIfaceParent}} {{/futureIfaceParent}}{
 {{#asyncFunctions}}
     {{>function}};
 {{/asyncFunctions}}

scrooge-generator/src/main/scala/com/twitter/scrooge/backend/JavaGenerator.scala
@@ -244,4 +244,10 @@ class JavaGenerator(
   }
 
   def genBaseFinagleService = codify("Service<byte[], byte[]>")
+
+  def getParentFinagleService(p: ServiceParent): CodeFragment =
+    genID(SimpleID("FinagledService").addScope(getServiceParentID(p)))
+
+  def getParentFinagleClient(p: ServiceParent): CodeFragment =
+    genID(SimpleID("FinagledClient").addScope(getServiceParentID(p)))
 }

scrooge-core/src/main/scala/com/twitter/scrooge/ThriftService.scala
@@ -1,0 +1,7 @@
+package com.twitter.scrooge
+
+
+/**
+ * A marker trait for interfaces that represent thrift services.
+ */
+trait ThriftService

scrooge-generator/src/main/resources/javagen/function.java
@@ -1,2 +1,2 @@
 {{docstring}}
-public {{#async}}Future<{{/async}}{{typeName}}{{#async}}>{{/async}} {{funcName}}({{fieldParams}}){{#hasThrows}} throws {{#throws}}{{typeName}}{{/throws|, }}{{/hasThrows}}
+public {{#generic}}{{generic}}<{{/generic}}{{typeName}}{{#generic}}>{{/generic}} {{funcName}}({{fieldParams}}){{#hasThrows}} throws {{#throws}}{{typeName}}{{/throws|, }}{{/hasThrows}}

scrooge-generator/src/main/resources/scalagen/finagleClient.scala
@@ -1,12 +1,29 @@
+package {{package}}
+
+import com.twitter.finagle.SourcedException
+import com.twitter.finagle.stats.{NullStatsReceiver, StatsReceiver}
+import com.twitter.finagle.thrift.ThriftClientRequest
+import com.twitter.finagle.{Service => FinagleService}
+import com.twitter.scrooge.{ThriftStruct, ThriftStructCodec}
+import com.twitter.util.Future
+import java.nio.ByteBuffer
+import java.util.Arrays
+import org.apache.thrift.protocol._
+import org.apache.thrift.TApplicationException
+import org.apache.thrift.transport.{TMemoryBuffer, TMemoryInputTransport}
+import scala.collection.mutable
+import scala.collection.{Map, Set}
+
 {{docstring}}
-class FinagledClient(
+@javax.annotation.Generated(value = Array("com.twitter.scrooge.Compiler"), date = "{{date}}")
+class {{ServiceName}}$FinagleClient(
   {{#hasParent}}override {{/hasParent}}val service: FinagleService[ThriftClientRequest, Array[Byte]],
   {{#hasParent}}override {{/hasParent}}val protocolFactory: TProtocolFactory = new TBinaryProtocol.Factory,
   {{#hasParent}}override {{/hasParent}}val serviceName: String = "",
   stats: StatsReceiver = NullStatsReceiver
-) extends {{#hasParent}}{{finagleClientParent}}(service, protocolFactory, serviceName, stats) with {{/hasParent}}FutureIface {
+) extends {{#hasParent}}{{finagleClientParent}}(service, protocolFactory, serviceName, stats) with {{/hasParent}}{{ServiceName}}[Future] {
+  import {{ServiceName}}._
 {{^hasParent}}
-  // ----- boilerplate that should eventually be moved into finagle:
 
   protected def encodeRequest(name: String, args: ThriftStruct) = {
     val buf = new TMemoryBuffer(512)

scrooge-generator/src/main/resources/scalagen/finagleService.scala
@@ -1,11 +1,24 @@
+package {{package}}
+
+import com.twitter.finagle.{Service => FinagleService}
+import com.twitter.scrooge.ThriftStruct
+import com.twitter.util.Future
+import java.nio.ByteBuffer
+import java.util.Arrays
+import org.apache.thrift.protocol._
+import org.apache.thrift.TApplicationException
+import org.apache.thrift.transport.{TMemoryBuffer, TMemoryInputTransport}
+import scala.collection.mutable
+import scala.collection.{Map, Set}
 
 {{docstring}}
-class FinagledService(
-  iface: FutureIface,
+@javax.annotation.Generated(value = Array("com.twitter.scrooge.Compiler"), date = "{{date}}")
+class {{ServiceName}}$FinagleService(
+  iface: {{ServiceName}}[Future],
   protocolFactory: TProtocolFactory
 ) extends {{finagleServiceParent}}{{#hasParent}}(iface, protocolFactory){{/hasParent}} {
+  import {{ServiceName}}._
 {{^hasParent}}
-  // ----- boilerplate that should eventually be moved into finagle:
 
   protected val functionMap = new mutable.HashMap[String, (TProtocol, Int) => Future[Array[Byte]]]()
 

scrooge-generator/src/main/resources/scalagen/function.scala
@@ -2,4 +2,4 @@
 {{#throws}}
 @throws(classOf[{{typeName}}])
 {{/throws}}
-def {{funcName}}({{fieldParams}}): {{#async}}Future[{{/async}}{{typeName}}{{#async}}]{{/async}}
+def {{funcName}}({{fieldParams}}): {{#generic}}{{generic}}[{{/generic}}{{typeName}}{{#generic}}]{{/generic}}

scrooge-generator/src/main/resources/scalagen/ostrichService.scala
@@ -1,5 +1,25 @@
+package {{package}}
+
+import com.twitter.conversions.time._
+import com.twitter.finagle.builder.{Server, ServerBuilder}
+import com.twitter.finagle.stats.StatsReceiver
+import com.twitter.finagle.stats.{StatsReceiver, OstrichStatsReceiver}
+import com.twitter.finagle.thrift.ThriftServerFramedCodec
+import com.twitter.finagle.tracing.{NullTracer, Tracer}
+import com.twitter.logging.Logger
+import com.twitter.ostrich
+import com.twitter.util.{Duration, Future}
+import java.net.InetSocketAddress
+import java.util.concurrent.atomic.AtomicReference
+import org.apache.thrift.protocol.{TBinaryProtocol, TProtocolFactory}
+import org.apache.thrift.transport.TTransport
+
+{{docstring}}
 @deprecated("long-term, use twitter-server, short term, use com.twitter.scrooge.OstrichThriftServer", "3.3.3")
-trait ThriftServer extends Service with FutureIface {
+@javax.annotation.Generated(value = Array("com.twitter.scrooge.Compiler"), date = "{{date}}")
+trait {{ServiceName}}$ThriftServer extends ostrich.admin.Service
+  with {{ServiceName}}[Future]
+{
   val log = Logger.get(getClass)
 
   def thriftCodec = ThriftServerFramedCodec()
@@ -17,7 +37,7 @@ trait ThriftServer extends Service with FutureIface {
   def server_=(value: Server) = _server.set(value)
 
   def start() {
-    val thriftImpl = new FinagledService(this, thriftProtocolFactory)
+    val thriftImpl = new {{ServiceName}}$FinagleService(this, thriftProtocolFactory)
     server_=(serverBuilder.build(thriftImpl))
   }
 

commit 73ad8b6ad615cbb3933468682da5203dd0de8eb7

Generate FinagledClient and FinagledService for java-experimental
Support was probably broken in commit cad09af796 when
the *Client and *Service classes were moved out of the "main"
service file. This change was reflected in the generated Scala code,
but not in the Java code.

See also: https://groups.google.com/forum/#!topic/finaglers/W4hhEcAbocI

- Configure compiler to output generated *Client and *Service classes
  to file
- Rewrite relevant Java generators so that they generate code
  compatible with a class living in its own file
  (import statements, remove "static" keyword etc)
- The "main" service class has a FinagledClient which extends
  (...ServiceName...)$FinagleClient, like in the Scala generated code.

scrooge-generator/src/main/resources/javagen/finagleClient.java
@@ -1,11 +1,46 @@
+package {{package}};
+
+import com.twitter.scrooge.Option;
+import com.twitter.scrooge.ThriftStruct;
+import com.twitter.scrooge.ThriftStructCodec;
+import com.twitter.scrooge.ThriftStructCodec3;
+import com.twitter.scrooge.Utilities;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import org.apache.thrift.protocol.*;
+import org.apache.thrift.TApplicationException;
+import com.twitter.finagle.Service;
+import com.twitter.finagle.SourcedException;
+import com.twitter.finagle.stats.Counter;
+import com.twitter.finagle.stats.NullStatsReceiver;
+import com.twitter.finagle.stats.StatsReceiver;
+import com.twitter.finagle.thrift.ThriftClientRequest;
+import com.twitter.util.Function2;
+import com.twitter.util.Function;
+import com.twitter.util.Future;
+import com.twitter.util.FutureEventListener;
+import java.util.Arrays;
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TMemoryBuffer;
+import org.apache.thrift.transport.TMemoryInputTransport;
+import org.apache.thrift.transport.TTransport;
+
+import {{package}}.{{ServiceName}}.*;
+
 {{docstring}}
-public static class FinagledClient{{#hasParent}} extends {{finagleClientParent}}{{/hasParent}} implements FutureIface {
+public class {{ServiceName}}$FinagleClient{{#hasParent}} extends {{finagleClientParent}}{{/hasParent}} implements {{ServiceName}}.FutureIface {
   private com.twitter.finagle.Service<ThriftClientRequest, byte[]> service;
   private String serviceName;
   private TProtocolFactory protocolFactory /* new TBinaryProtocol.Factory */;
   private StatsReceiver scopedStats;
 
-  public FinagledClient(
+  public {{ServiceName}}$FinagleClient(
     com.twitter.finagle.Service<ThriftClientRequest, byte[]> service,
     TProtocolFactory protocolFactory /* new TBinaryProtocol.Factory */,
     String serviceName,
@@ -50,7 +85,7 @@ public static class FinagledClient{{#hasParent}} extends {{finagleClientParent}}
       if (msg.type == TMessageType.EXCEPTION) {
         TException exception = TApplicationException.read(iprot);
         if (exception instanceof SourcedException) {
-          if (this.serviceName != "") ((SourcedException) exception).serviceName_$eq(this.serviceName);
+          if (this.serviceName != "") ((SourcedException) exception).serviceName_$eq({{ServiceName}}$FinagleClient.this.serviceName);
         }
         throw exception;
       } else {
@@ -73,7 +108,7 @@ public static class FinagledClient{{#hasParent}} extends {{finagleClientParent}}
     public StatsReceiver failuresScope;
 
     public __Stats(String name) {
-      StatsReceiver scope = FinagledClient.this.scopedStats.scope(name);
+      StatsReceiver scope = {{ServiceName}}$FinagleClient.this.scopedStats.scope(name);
       this.requestsCounter = scope.counter0("requests");
       this.successCounter = scope.counter0("success");
       this.failuresCounter = scope.counter0("failures");

scrooge-generator/src/main/resources/javagen/finagleClientFunction.java
@@ -38,7 +38,7 @@ private __Stats {{__stats_name}}() {
   }).rescue(new Function<Throwable, Future<{{type}}>>() {
     public Future<{{type}}> apply(Throwable t) {
       if (t instanceof SourcedException) {
-        ((SourcedException) t).serviceName_$eq(FinagledClient.this.serviceName);
+        ((SourcedException) t).serviceName_$eq({{ServiceName}}$FinagleClient.this.serviceName);
       }
       return Future.exception(t);
     }

scrooge-generator/src/main/resources/javagen/finagleService.java
@@ -1,9 +1,44 @@
+package {{package}};
+
+import com.twitter.scrooge.Option;
+import com.twitter.scrooge.ThriftStruct;
+import com.twitter.scrooge.ThriftStructCodec;
+import com.twitter.scrooge.ThriftStructCodec3;
+import com.twitter.scrooge.Utilities;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import org.apache.thrift.protocol.*;
+import org.apache.thrift.TApplicationException;
+import com.twitter.finagle.Service;
+import com.twitter.finagle.SourcedException;
+import com.twitter.finagle.stats.Counter;
+import com.twitter.finagle.stats.NullStatsReceiver;
+import com.twitter.finagle.stats.StatsReceiver;
+import com.twitter.finagle.thrift.ThriftClientRequest;
+import com.twitter.util.Function2;
+import com.twitter.util.Function;
+import com.twitter.util.Future;
+import com.twitter.util.FutureEventListener;
+import java.util.Arrays;
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TMemoryBuffer;
+import org.apache.thrift.transport.TMemoryInputTransport;
+import org.apache.thrift.transport.TTransport;
+
+import {{package}}.{{ServiceName}}.*;
+
 {{docstring}}
-public static class FinagledService extends {{finagleServiceParent}} {
-  final private FutureIface iface;
+public class {{ServiceName}}$FinagleService extends {{finagleServiceParent}} {
+  final private {{ServiceName}}.FutureIface iface;
   final private TProtocolFactory protocolFactory;
 
-  public FinagledService(final FutureIface iface, final TProtocolFactory protocolFactory) {
+  public {{ServiceName}}$FinagleService(final {{ServiceName}}.FutureIface iface, final TProtocolFactory protocolFactory) {
 {{#hasParent}}
     super(iface, protocolFactory);
 {{/hasParent}}

scrooge-generator/src/main/resources/javagen/service.java
@@ -48,6 +48,27 @@ public class {{ServiceName}} {
     {{>function}};
 {{/asyncFunctions}}
   }
+
+  class FinagledClient extends {{ServiceName}}$FinagleClient {
+      public FinagledClient (
+        com.twitter.finagle.Service<com.twitter.finagle.thrift.ThriftClientRequest, byte[]> service,
+        TProtocolFactory protocolFactory,
+        String serviceName,
+        com.twitter.finagle.stats.StatsReceiver stats
+        ) {
+          super(service, protocolFactory, serviceName, stats);
+        }
+  }
+
+  class FinagledService extends {{ServiceName}}$FinagleService {
+      public FinagledService (
+        FutureIface iface,
+        TProtocolFactory protocolFactory
+        ) {
+          super(iface, protocolFactory);
+      }
+  }
+
 {{/withFinagle}}
 
 {{#internalStructs}}

scrooge-generator/src/main/scala/com/twitter/scrooge/backend/JavaGenerator.scala
@@ -16,6 +16,8 @@ package com.twitter.scrooge.backend
  * limitations under the License.
  */
 
+import java.io.File
+
 import com.twitter.scrooge.ast._
 import com.twitter.scrooge.mustache.Dictionary._
 import com.twitter.scrooge.frontend.{ScroogeInternalException, ResolvedDocument}
@@ -262,4 +264,22 @@ class JavaGenerator(
 
   def getParentFinagleClient(p: ServiceParent): CodeFragment =
     genID(SimpleID("FinagledClient").addScope(getServiceParentID(p)))
+
+  override def finagleClientFile(
+    packageDir: File,
+    service: Service, options:
+    Set[ServiceOption]
+  ): Option[File] =
+    options.find(_ == WithFinagle) map { _ =>
+      new File(packageDir, service.sid.toTitleCase.name + "$FinagleClient" + fileExtension)
+    }
+
+  override def finagleServiceFile(
+     packageDir: File,
+     service: Service, options:
+    Set[ServiceOption]
+  ): Option[File] =
+    options.find(_ == WithFinagle) map { _ =>
+      new File(packageDir, service.sid.toTitleCase.name + "$FinagleService" + fileExtension)
+    }
 }