6d7ed5761c3a35c763b33cf6212672c3d8fb1df0
eirslett <eirik.sletteberg@finn.no>
2014-08-17 20:07:04 +0200

    Generated Java code should read/write enums correctly
    Problem:
    When reading/writing enums over the wire, they are defined
    as TType.I32, and not TType.ENUM, as one would expect.
    This is arguably a problem with the Thrift spec, but we must
    adhere to it anyways.
    
    Solution:
    The quirk was already fixed in the generated Scala code.
    This ports the enum fix from theScala code generator to
    the Java code generator

--- scrooge-generator/src/main/resources/javagen/readField.java
+++ scrooge-generator/src/main/resources/javagen/readField.java
@@ -1,6 +1,12 @@
 case {{id}}: /* {{fieldName}} */
   switch (_field.type) {
+{{#isEnum}}
+    case TType.I32:
+    case TType.ENUM:
+{{/isEnum}}
+{{^isEnum}}
     case TType.{{constType}}:
+{{/isEnum}}
       {{fieldType}} {{valueVariableName}};
       {{>readValue}}
       {{fieldName}} = {{valueVariableName}};
--- scrooge-generator/src/main/resources/javagen/writeField.java
+++ scrooge-generator/src/main/resources/javagen/writeField.java
@@ -1,5 +1,5 @@
 {{#optional}}if ({{fieldName}}.isDefined()) {{{/optional}}
-  _oprot.writeFieldBegin({{fieldConst}});
+  _oprot.writeFieldBegin({{fieldConst}}{{#isEnum}}I32{{/isEnum}});
   {{>qualifiedFieldType}} {{valueVariableName}} = {{fieldName}}{{#optional}}.get(){{/optional}};
   {{>writeValue}}
   _oprot.writeFieldEnd();
