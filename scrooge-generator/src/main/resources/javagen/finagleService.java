3ca7f61065828b66da3afd6d5dcbecdfe4c5ac7a
eirslett <eirik.sletteberg@finn.no>
2014-08-23 11:13:34 +0200

    Reorder imports

--- scrooge-generator/src/main/resources/javagen/finagleService.java
+++ scrooge-generator/src/main/resources/javagen/finagleService.java
@@ -1,35 +1,37 @@
 package {{package}};
 
-import com.twitter.scrooge.Option;
-import com.twitter.scrooge.ThriftStruct;
-import com.twitter.scrooge.ThriftStructCodec;
-import com.twitter.scrooge.ThriftStructCodec3;
-import com.twitter.scrooge.Utilities;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
-import org.apache.thrift.protocol.*;
-import org.apache.thrift.TApplicationException;
 import com.twitter.finagle.Service;
 import com.twitter.finagle.SourcedException;
 import com.twitter.finagle.stats.Counter;
 import com.twitter.finagle.stats.NullStatsReceiver;
 import com.twitter.finagle.stats.StatsReceiver;
 import com.twitter.finagle.thrift.ThriftClientRequest;
-import com.twitter.util.Function2;
 import com.twitter.util.Function;
+import com.twitter.util.Function2;
 import com.twitter.util.Future;
 import com.twitter.util.FutureEventListener;
-import java.util.Arrays;
+import com.twitter.scrooge.Option;
+import com.twitter.scrooge.ThriftStruct;
+import com.twitter.scrooge.ThriftStructCodec;
+import com.twitter.scrooge.ThriftStructCodec3;
+import com.twitter.scrooge.Utilities;
+
+import org.apache.thrift.TApplicationException;
 import org.apache.thrift.TException;
+import org.apache.thrift.protocol.*;
 import org.apache.thrift.transport.TMemoryBuffer;
 import org.apache.thrift.transport.TMemoryInputTransport;
 import org.apache.thrift.transport.TTransport;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import {{package}}.{{ServiceName}}.*;
 
