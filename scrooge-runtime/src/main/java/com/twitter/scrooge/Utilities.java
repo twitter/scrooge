9cd372b86a235cc9fa5ab344829572e1a6fa3dd6
Nik Shkrob <nshkrob@twitter.com>
2014-09-18 20:58:35 +0000

    [split] scrooge: Remove SafeVarargs for JDK 6 compatibility.
    RB_ID=459647

--- scrooge-runtime/src/main/java/com/twitter/scrooge/Utilities.java
+++ scrooge-runtime/src/main/java/com/twitter/scrooge/Utilities.java
@@ -21,12 +21,10 @@ public class Utilities {
     }
   }
 
-  @SafeVarargs
   public static <T> List<T> makeList(T... elements) {
     return Arrays.asList(elements);
   }
 
-  @SafeVarargs
   public static <A, B> Map<A, B> makeMap(Tuple<A, B>... elements) {
     Map<A, B> map = new HashMap<A, B>();
     for (Tuple<A, B> element : elements) {
@@ -35,7 +33,6 @@ public class Utilities {
     return map;
   }
 
-  @SafeVarargs
   public static <T> Set<T> makeSet(T... elements) {
     Set<T> set = new HashSet<T>();
     for (T element : elements) {
