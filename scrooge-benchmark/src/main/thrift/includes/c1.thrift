cdb6bea8b35e01f90d715f8b84fe89347accb76e
Nik Shkrob <nshkrob@twitter.com>
2014-10-03 11:43:28 -0700

    Move includes benchmark into generated thrift.

--- scrooge-benchmark/src/main/thrift/includes/c1.thrift
+++
@@ -1,7 +0,0 @@
-
-include "b1.thrift"
-
-
-service MyService1 {
-  b1.MyEnum getEnumValue(1: i32 aaaa )
-}
--- scrooge-benchmark/src/main/thrift/includes/top.thrift
+++
@@ -1,5 +0,0 @@
-include "c1.thrift"
-include "c2.thrift"
-include "c3.thrift"
-
-service TopService { i32 getNumber(1: string someInput) }
