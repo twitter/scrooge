eb57ae5452b3a7a1fe18ba3ebf6bcc0721393d8c
Christopher Coco <ccoco@twitter.com>
2016-03-10 23:14:38 +0000

    Bump versions - util/ostrich/finagle/scrooge/twitter-server
    Prepare libraries for release.
    
    RB_ID=809587

--- project/Build.scala
+++ project/Build.scala
@@ -15,12 +15,12 @@ object Scrooge extends Build {
   val branch = Process("git" :: "rev-parse" :: "--abbrev-ref" :: "HEAD" :: Nil).!!.trim
   val suffix = if (branch == "master") "" else "-SNAPSHOT"
 
-  val libVersion = "4.5.0" + suffix
+  val libVersion = "4.6.0" + suffix
 
   // To build the develop branch you need to publish util, and finagle locally:
   // 'git checkout develop; sbt publishLocal' to publish SNAPSHOT versions of these projects.
-  val utilVersion = "6.32.0" + suffix
-  val finagleVersion = "6.33.0" + suffix
+  val utilVersion = "6.33.0" + suffix
+  val finagleVersion = "6.34.0" + suffix
 
   val libthriftVersion = "0.5.0-1"
 
