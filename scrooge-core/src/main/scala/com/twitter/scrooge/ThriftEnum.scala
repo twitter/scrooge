1810f5cc99ed62d0210fffd9748ad77d873a6f55
Steven She <steven.she@human.x.ai>
2016-03-18 22:22:34 -0400

    Add return type to originalName, name, and value

--- scrooge-core/src/main/scala/com/twitter/scrooge/ThriftEnum.scala
+++ scrooge-core/src/main/scala/com/twitter/scrooge/ThriftEnum.scala
@@ -2,9 +2,15 @@ package com.twitter.scrooge
 
 import org.apache.thrift.TEnum
 
+
 trait ThriftEnum extends TEnum {
   def value: Int
   def name: String
+
+  /**
+    * The original name for the enum value as defined in the input Thrift IDL file.
+    */
   def originalName: String
+
   def getValue = value
 }
