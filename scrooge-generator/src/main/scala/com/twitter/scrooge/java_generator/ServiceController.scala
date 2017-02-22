40b940803b06ef86e6c7a437ef0324fb5164cadc
Jillian Crossley <jillianc@twitter.com>
2017-02-22 23:38:22 +0000

    scrooge: Fully qualify ProcessFunction in scrooge-generator
    Problem
    
    To get everything working on Scala 2.12, we need to fully qualify the ProcessFunction
    interface scrooge-generator produces.
    
    Solution
    
    Use "parent_name" as the name of the parent, instead of "name"; "name" is already bound in
    the scope of "functions" and thus the parent name is inaccessible without this change. Use
    the "parent_name" to qualify the ProcessFunction implementation.
    
    RB_ID=908670

--- scrooge-generator/src/main/scala/com/twitter/scrooge/java_generator/ServiceController.scala
+++ scrooge-generator/src/main/scala/com/twitter/scrooge/java_generator/ServiceController.scala
@@ -6,8 +6,12 @@ import com.twitter.scrooge.ast.Service
 class ServiceController(service: Service, generator: ApacheJavaGenerator, ns: Option[Identifier])
   extends TypeController(service, generator, ns) {
   val extends_iface = service.parent match {
-    case Some(parent) => Map("name" -> generator.qualifyNamedType(parent.sid, parent.filename).fullName)
-    case None => false
+    case Some(parent) => 
+      Map(
+        "parent_name" ->
+        generator.qualifyNamedType(parent.sid, parent.filename).fullName)
+    case None =>
+      false
   }
   val functions = service.functions map { f => new FunctionController(f, generator, ns) }
 }
