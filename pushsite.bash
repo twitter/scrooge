b9f7a902e396ba7b0bc48320839f16e84a6683c6
Moses Nakamura <mnakamura@twitter.com>
2017-11-10 19:45:33 +0000

    csl: Updated pushsites scripts for sbt 1.0.x
    Summary: Problem / Solution
    
    We need to switch to camelCase and away from kebab-case to get sbt 1.0.x
    working.
    
    JIRA Issues: CSL-5504
    
    Differential Revision: https://phabricator.twitter.biz/D110255

--- pushsite.bash
+++ pushsite.bash
@@ -6,7 +6,7 @@ dir=/tmp/scrooge.$$
 trap "rm -fr $dir" 0 1 2
 
 echo 'making site...' 1>&2
-./sbt --warn scrooge-doc/make-site
+./sbt --warn scrooge-doc/makeSite
 
 echo 'cloning...' 1>&2
 git clone -b gh-pages git@github.com:twitter/scrooge.git $dir >/dev/null 2>&1
