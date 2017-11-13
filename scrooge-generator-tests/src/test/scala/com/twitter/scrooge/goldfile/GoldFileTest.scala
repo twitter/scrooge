0aeaa66dd6e861931f24999c6aa4be99fe329906
Dave Benjamin <daveb@twitter.com>
2017-11-13 17:02:53 +0000

    scrooge-generator-tests: Enable overriding diff behavior in GoldFileTest
    Summary: Problem
    
    Scrooge's GoldFileTest base class for tests assumes that gold files
    will be text-based, and there is currently no way to reuse it for
    binary output.
    
    Solution
    
    This change moves the part of the test that does diffing into a new
    protected "diff" method that can be overridden by subclasses. This
    method takes InputStream parameters for the generated and gold file
    content. This method also takes file name and relative path parameters
    so that error messages can still include this path information.
    
    Result
    
    There should be no change to existing gold file tests, but subclasses
    can now override the "diff" method to customize diff behavior.
    
    JIRA Issues: SSTR-1764
    
    Differential Revision: https://phabricator.twitter.biz/D106725

--- scrooge-generator-tests/src/test/scala/com/twitter/scrooge/goldfile/GoldFileTest.scala
+++ scrooge-generator-tests/src/test/scala/com/twitter/scrooge/goldfile/GoldFileTest.scala
@@ -3,7 +3,7 @@ package com.twitter.scrooge.goldfile
 import com.twitter.io.Files
 import com.twitter.scrooge.Main
 import com.twitter.scrooge.testutil.TempDirectory
-import java.io.File
+import java.io.{ByteArrayInputStream, File, InputStream}
 import java.util.regex.Pattern
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import scala.io.Source
@@ -91,70 +91,75 @@ abstract class GoldFileTest extends FunSuite
   protected def testThriftFiles = Seq("gold_file_input/gold.thrift")
   protected def goldFilesRoot: String = s"gold_file_output_$language"
 
-  test("generated output looks as expected") {
-    val ccl = Thread.currentThread().getContextClassLoader
-
-    def generatedDataFor(file: File): String = {
-      val gen = Source.fromFile(file, "UTF-8").mkString
-
-      // normalize the headers
-      val headersNormalized = headerRegEx.replaceAllIn(gen, headerNormalizedReplacement)
-
-      trailingSpaces.matcher(headersNormalized).replaceAll("")
-    }
-
-    def goldDataFor(suffix: String): String = {
-      val path = s"$goldFilesRoot/$suffix"
-      val is = ccl.getResourceAsStream(path)
-      if (is == null)
-        return ""
-      Source.fromInputStream(is, "UTF-8").mkString
-    }
-
-    generatedFiles.foreach { gen =>
-      // we want to take the path after tempDir and compare to suffixed gold file
-      // in our resources dir.
-      // the +1 removes what would be a leading slash from suffix
-      val suffix = gen.toString.drop(tempDir.toString.length + 1)
-
-      val genStr = generatedDataFor(gen)
-      val expected = goldDataFor(suffix)
-      withClue(suffix) {
-        if (genStr != expected) {
-          val diff = {
-            var i = 0
-            while (i < math.min(genStr.length, expected.length) && genStr(i) == expected(i)) {
-              i += 1
-            }
-            val surround = 50
-            val longerStr = if (genStr.length >= expected.length) genStr else expected
-            val substring =
-              longerStr.substring(math.max(0, i - surround), i) ++
-              s"|->${longerStr(i)}<-|" ++
-              longerStr.substring(math.min(i + 1, longerStr.length), math.min(i + surround, longerStr.length))
-
-            s"The difference is at character $i: " +
-              s"($substring). line: ${ longerStr.substring(0, i).count(_ == '\n') + 1 }"
-          }
-
-          val msg =
-            s"""
-               |The generated file ${gen.getName} did not match gold file
-               |"$goldFilesRoot/$suffix".
-               |Generated string is ${genStr.length} characters long
-               |Expected string is ${expected.length} characters long
-               |
-               |$diff
-               |
-               |Compare the output in stdout to the gold file and
-               |either fix the generator or update the gold file to match.
-             """.stripMargin
-          println(msg)
-          println(s"Generated file $suffix:\n$genStr<<<EOF")
-          fail(msg)
+  protected def diff(gold: InputStream, gen: InputStream, genFileName: String, genRelPath: String): Unit = {
+    val genStr = generatedDataFor(gen)
+    val expected = inputStreamToString(gold)
+    if (genStr != expected) {
+      val diff = {
+        var i = 0
+        while (i < math.min(genStr.length, expected.length) && genStr(i) == expected(i)) {
+          i += 1
         }
+        val surround = 50
+        val longerStr = if (genStr.length >= expected.length) genStr else expected
+        val substring =
+          longerStr.substring(math.max(0, i - surround), i) ++
+          s"|->${longerStr(i)}<-|" ++
+          longerStr.substring(math.min(i + 1, longerStr.length), math.min(i + surround, longerStr.length))
+
+        s"The difference is at character $i: " +
+          s"($substring). line: ${ longerStr.substring(0, i).count(_ == '\n') + 1 }"
       }
+
+      val msg =
+        s"""
+           |The generated file ${genFileName} did not match gold file
+           |"$goldFilesRoot/$genRelPath".
+           |Generated string is ${genStr.length} characters long
+           |Expected string is ${expected.length} characters long
+           |
+           |$diff
+           |
+           |Compare the output in stdout to the gold file and
+           |either fix the generator or update the gold file to match.
+         """.stripMargin
+      println(msg)
+      println(s"Generated file $genRelPath:\n$genStr<<<EOF")
+      fail(msg)
     }
   }
 
+  private def generatedDataFor(is: InputStream): String = {
+    val gen = inputStreamToString(is)
+
+    // normalize the headers
+    val headersNormalized = headerRegEx.replaceAllIn(gen, headerNormalizedReplacement)
+
+    trailingSpaces.matcher(headersNormalized).replaceAll("")
+  }
+
+  private def inputStreamToString(is: InputStream): String = {
+    if (is == null) {
+      ""
+    } else {
+      Source.fromInputStream(is, "UTF-8").mkString
+    }
+  }
+
+  test("generated output looks as expected") {
+    val ccl = Thread.currentThread().getContextClassLoader
+
+    generatedFiles.foreach { gen =>
+      // We want to take the path after tempDir and compare to the gold file
+      // in our resources dir. The +1 removes what would be a leading slash
+      // from the relative path.
+      val genRelPath = gen.toString.drop(tempDir.toString.length + 1)
+
+      withClue(genRelPath) {
+        val genStream = new ByteArrayInputStream(Files.readBytes(gen))
+        val goldStream = ccl.getResourceAsStream(s"$goldFilesRoot/$genRelPath")
+        diff(goldStream, genStream, gen.getName, genRelPath)
+      }
+    }
+  }
 }
