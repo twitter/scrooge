c423e75343a49b7fd2222fb9b10521507da31462
Ryan O'Neill <ryano@twitter.com>
2019-11-11 22:03:40 +0000

    scrooge: Get scrooge-maven-demo Working Again
    Problem / Solution
    
    The scrooge-maven-demo provides an example of how to compile Thrift
    IDL when a Scala (or Java) project uses maven. The current version
    is broken. Let's fix it.
    
    JIRA Issues: CSL-9066
    
    Differential Revision: https://phabricator.twitter.biz/D397479

--- demos/scrooge-maven-demo/src/main/scala/com/twitter/example/Demo.scala
+++ demos/scrooge-maven-demo/src/main/scala/com/twitter/example/Demo.scala
@@ -1,30 +1,24 @@
 package com.twitter.example
 
-import com.twitter.finagle.Thrift
-import com.twitter.finagle.builder.{ServerBuilder, ClientBuilder}
+import com.twitter.finagle.{ListeningServer, Thrift}
 import com.twitter.mydemo.renamed.{User, UserService}
 import com.twitter.util.Future
-import java.net.{SocketAddress, InetSocketAddress}
 import java.util.concurrent.atomic.AtomicInteger
-import org.apache.thrift.protocol.TBinaryProtocol
-import com.twitter.finagle.ListeningServer
 
 object DemoClient {
-  def buildClient(address: SocketAddress): UserService.FinagledClient = {
-    val clientService = ClientBuilder()
-      .hosts(address.asInstanceOf[InetSocketAddress])
-      .stack(Thrift.client)
-      .hostConnectionLimit(1)
-      .build()
-    new UserService.FinagledClient(clientService)
-  }
+
+  def buildClient(port: Int): UserService.MethodPerEndpoint =
+    Thrift.client
+      .withLabel("demo-client")
+      .build[UserService.MethodPerEndpoint](s"localhost:$port")
+
 }
 
 object DemoServer {
   private val userIdCounter: AtomicInteger = new AtomicInteger(0)
 
   // need to provide an implementation for the finagle service
-  class MyUserImpl extends UserService.FutureIface {
+  class MyUserImpl extends UserService.MethodPerEndpoint {
     def createUser(name: String): Future[User] = {
       val id = userIdCounter.incrementAndGet()
       Future.value(User(id, name))
@@ -32,12 +26,10 @@ object DemoServer {
   }
 
   def buildServer(): ListeningServer = {
-    val protocol = new TBinaryProtocol.Factory()
-    val serverService = new UserService.FinagledService(new MyUserImpl, protocol)
-    ServerBuilder()
-      .stack(Thrift.server)
-      .name("binary_service")
-      .bindTo(new InetSocketAddress(0))
-      .build(serverService)
+    val userService = new MyUserImpl
+    Thrift.server
+      .withLabel("demo-server")
+      .serveIface("localhost:0", userService)
   }
+
 }
---
+++ demos/scrooge-maven-demo/src/test/scala/com/twitter/example/DemoTest.scala
@@ -0,0 +1,28 @@
+package com.twitter.example
+
+import com.twitter.conversions.DurationOps._
+import com.twitter.mydemo.renamed.User
+import com.twitter.util.{Future, Await}
+import java.net.{InetSocketAddress, SocketAddress}
+import org.scalatest.FunSuite
+
+class DemoTest extends FunSuite {
+  def printUser(user: User): Unit = println("User %s, id %d".format(user.name, user.id))
+
+  def await[A](f: Future[A]): A = Await.result(f, 5.seconds)
+
+  def getPort(address: SocketAddress): Int =
+    address.asInstanceOf[InetSocketAddress].getPort
+
+  test("generated finagle client and server") {
+    val server = DemoServer.buildServer()
+    val client = DemoClient.buildClient(getPort(server.boundAddress))
+
+    await(client.createUser("Steph")).name == "Steph"
+    await(client.createUser("Klay")).name == "Klay"
+
+    client.asClosable.close(5.seconds)
+    server.close(5.seconds)
+  }
+
+}
