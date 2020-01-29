ff69cf4f5886fa9959b742d5f75eac79d67a0074
chenzhuoyu <chenzhuoyu@bytedance.com>
2020-01-29 15:32:51 +0800

    fix: use FQDN for calling enum's "findByValue"

---
+++ scrooge-generator-tests/src/test/resources/test_thrift/struct_field_with_same_name_and_type.thrift
@@ -0,0 +1,10 @@
+namespace java test_enum
+
+enum OrganizationType {
+    TYPE_A = 1
+    TYPE_B = 2
+}
+
+struct Organization {
+    1: OrganizationType OrganizationType
+}
--- scrooge-generator-tests/src/test/scala/com/twitter/scrooge/java_generator/ApacheJavaGeneratorSpec.scala
+++ scrooge-generator-tests/src/test/scala/com/twitter/scrooge/java_generator/ApacheJavaGeneratorSpec.scala
@@ -256,7 +256,6 @@ class ApacheJavaGeneratorSpec extends Spec {
         result must be(List("No fields set for union type 'DeepValidationUnion'."))
       }
     }
-  }
 
   "passthrough fields" should {
     "passthroughStruct" in {
@@ -330,4 +329,11 @@ class ApacheJavaGeneratorSpec extends Spec {
 
   }
 
+    "generate enum with fully-qualified class name" in {
+      val doc = generateDoc(getFileContents("test_thrift/struct_field_with_same_name_and_type.thrift"))
+      val gen = getGenerator(doc)
+      val ctrl = new StructController(doc.structs(0), Set(), false, gen, doc.namespace("java"))
+      gen.renderMustache("struct.mustache", ctrl).trim must include ("test_enum.OrganizationType.findByValue(iprot.readI32())")
+    }
+  }
 }
