bfead3cdcdf6e43e2fda26a1e332239fab3ba180
Benjamin Navetta <bnavetta@twitter.com>
2022-10-26 17:56:05 +0000

    scrooge: Support binary files and includes in generator tests
    Problem
    
    Recent changes to the `GoldFileTest` API assume that all Scrooge generators
    produce text files which can be normalized. This prevents testing binary
    formats (for example, serialized schema representations). In addition, the
    `Utils.getIncludeFilePath` adapter does not handle Bazel Thrift inputs that
    import other files, because the imported files are not extracted from the jar
    on the classpath.
    
    Solution
    
    To handle binary files in `GoldFileTest`, we can introduce two overridable
    adapter methods, `diffBytes` and `normalizeHeaders`. Both accept byte arrays
    and delegate to the existing String-based methods. Subclasses that use binary
    files can override them with format-specific implementations.
    
    To handle imports, `Utils.getIncludeFilePath` can extract the entire directory
    when it encounters a resouce in a jar file. Because jar files are supported by
    the NIO `FileSystems` API, we can do this fairly concisely.
    
    Result
    
    After this change, gold Thrift files that import other files will pass with
    Bazel, and generators that produce binary files can be tested.
    
    Differential Revision: https://phabricator.twitter.biz/D1001992

--- scrooge-generator-tests/src/test/scala/com/twitter/scrooge/testutil/Utils.scala
+++ scrooge-generator-tests/src/test/scala/com/twitter/scrooge/testutil/Utils.scala
@@ -1,14 +1,23 @@
 package com.twitter.scrooge.testutil
 
-import com.google.common.base.Charsets
-import com.google.common.io.CharStreams
-import com.twitter.io.ClasspathResource
 import com.twitter.io.StreamIO
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStreamReader
 import org.scalatest.matchers.must.Matchers
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.FileSystems
+import java.nio.file.FileSystemNotFoundException
+import java.nio.file.FileVisitOption
+import java.nio.file.FileVisitResult
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+import java.util.Arrays
+import java.util.Collections
+import java.io.ByteArrayOutputStream
 
 object Utils extends Matchers {
   def verify(
@@ -49,9 +58,18 @@ object Utils extends Matchers {
   def getFileContents(resource: String): String = {
     val ccl = Thread.currentThread().getContextClassLoader
     val is = ccl.getResourceAsStream(resource)
-    val br = new BufferedReader(new InputStreamReader(is, Charsets.UTF_8))
-    try CharStreams.toString(br)
-    finally br.close()
+    try streamToString(is)
+    finally is.close()
+  }
+
+  def streamToString(is: InputStream): String = {
+    new String(streamToBytes(is), StandardCharsets.UTF_8)
+  }
+
+  def streamToBytes(is: InputStream): Array[Byte] = {
+    val out = new ByteArrayOutputStream()
+    StreamIO.copy(is, out)
+    out.toByteArray()
   }
 
   private[this] val headerRegEx =
@@ -86,41 +104,64 @@ object Utils extends Matchers {
       case _ => file
     }
     // we get the resource file from the path
-    val inputThrift = ccl.getResource(path).getPath
+    val inputThrift = ccl.getResource(path).toURI
+    val protocol = inputThrift.getScheme.toLowerCase
 
-    // if the the file contains a jar this condition is for bazel since it loads resource as a jar
-    if (inputThrift.contains(".jar!")) {
-      // load the named resource from the classpath
-      // opened resource returns input stream
-      ClasspathResource.load(path) match {
-        case Some(inputStream) =>
-          val tempFolder = System.getProperty("java.io.tmpdir")
-          // creates a temporary thread-local folder for a block of code to execute in.
-          // The folder is recursively deleted after the test.
-          var folder: File = new File(tempFolder, "scrooge-test-" + System.currentTimeMillis)
-          while (!folder.mkdir()) {
-            folder = new File(tempFolder, "scrooge-test-" + System.currentTimeMillis)
+    // if resource URI scheme is `jar` this condition is for bazel since it loads resource as a jar
+    if (protocol == "jar") {
+      // In order to handle Thrift files that import other files, we need to extract the whole directory
+      val fs =
+        try {
+          FileSystems.getFileSystem(inputThrift)
+        } catch {
+          case _: FileSystemNotFoundException =>
+            FileSystems.newFileSystem(inputThrift, Collections.emptyMap[String, String])
+        }
+
+      Files
+        .walk(fs.getPath(path).getParent, FileVisitOption.FOLLOW_LINKS)
+        .filter(a => Files.isRegularFile(a))
+        .forEach { a =>
+          // Convert the path to a string before resolving because it belongs to a different filesystem than thriftInputRoots
+          val out = thriftInputsRoot.resolve(a.toString)
+          // We're likely to visit the same jar file multiple times for different Thrift inputs, so skip files that already exist
+          if (!Files.exists(out)) {
+            Files.createDirectories(out.getParent)
+            Files.copy(a, out)
+          }
+        }
+      thriftInputsRoot.resolve(path).toString
+    } else if (protocol == "file") {
+      inputThrift.getPath // this condition is for pants and sbt as they load resources from a file path
+    } else {
+      throw new RuntimeException(s"Unsupported protocol: $protocol")
+    }
+  }
+
+  /**
+   * Temporary directory that Thrift inputs from JAR files on the classpath are extracted into
+   */
+  private[this] lazy val thriftInputsRoot: Path = {
+    val root = Files.createTempDirectory("scrooge-thrift-inputs")
+    sys.addShutdownHook {
+      Files.walkFileTree(
+        root,
+        new SimpleFileVisitor[Path] {
+          override def visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult = {
+            Files.delete(file)
+            FileVisitResult.CONTINUE
           }
 
-          folder.deleteOnExit() // delete folder on exit
-
-          val tempPath = directory match {
-            case Some(dir) => s"${folder.getCanonicalPath}/$dir"
-            case _ => folder.getCanonicalPath
+          override def postVisitDirectory(dir: Path, exc: IOException): FileVisitResult = {
+            // If deleting a file in the directory failed, `exc` will ne non-null
+            if (exc == null) {
+              Files.delete(dir)
+              FileVisitResult.CONTINUE
+            } else throw exc
           }
-          val out = File.createTempFile(tempPath, file)
-          out.deleteOnExit()
-          // buffer the given input stream
-          val byteArrayOutputStream = StreamIO.buffer(inputStream)
-          val outputStream = new FileOutputStream(out, true)
-          // Writes the complete contents of this byte array output stream to the specified output stream argument,
-          byteArrayOutputStream.writeTo(outputStream)
-          out.getCanonicalPath
-
-        case _ => throw new IllegalArgumentException(s"couldn't find $path as a resource")
-      }
-    } else
-      inputThrift // this condition is for pants and sbt as they load resources from a file path
-
+        }
+      )
+    }
+    root
   }
 }
